fb85c580195cb99e92457535408fbe4de931103e
Juan Pablo <juanibarbo456@gmail.com>
2019-10-14 17:48:57 -0500

    1

---
+++ exercises/activity:print_special_characters.c
@@ -0,0 +1,7 @@
+/*c programming:getting started*/
+/*Activity:print special characters*/
+#include <stdio.h>
+int main(void){
+  printf("Dennis Ritchie said:\n\"the only way to learn a new programming languege is by writing programs in it.\"");
+  return 0;
+}
